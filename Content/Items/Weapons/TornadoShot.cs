4aae74e7acf9cde5a96681337faad93e3ed4c0e7
YasinAli-Allen <yasin_ali-allen@hotmail.com>
2024-05-16 01:34:19 +1000

    Implement main tornado shot logic
    - Adds a custom ammo type for ammo AI logic
    - Adds a custom bow for multi proj (since ts is trash without it)
    
    TODO:
    - Fix animation
    - Take a look over and clean up code
    - Add tooltip, cleanup sprite, etc

---
+++ Content/Items/Weapons/TornadoShot.cs
@@ -0,0 +1,117 @@
+using ExampleMod.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Weapons
+{
+	public class TornadoShot : ModItem
+	{
+		public override void SetDefaults() {
+			// Modders can use Item.DefaultToRangedWeapon to quickly set many common properties, such as: useTime, useAnimation, useStyle, autoReuse, DamageType, shoot, shootSpeed, useAmmo, and noMelee. These are all shown individually here for teaching purposes.
+			Item.DefaultToBow(12, 100, true);
+			Item.SetWeaponValues(11, 1f);
+
+			// Common Properties
+			Item.width = 62; // Hitbox width of the item.
+			Item.height = 32; // Hitbox height of the item.
+			Item.scale = 0.75f;
+			Item.rare = ItemRarityID.Green; // The color that the item's name will be in-game.
+
+			// The sound that this item plays when used.
+			Item.UseSound = new SoundStyle($"{nameof(ExampleMod)}/Assets/Sounds/Items/Guns/ExampleGun") {
+				Volume = 0.9f,
+				PitchVariance = 0.2f,
+				MaxInstances = 3,
+			};
+
+			Item.shoot = ModContent.ProjectileType<TornadoShotProjectile>();
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+
+		// This method lets you adjust position of the gun in the player's hands. Play with these values until it looks good with your graphics.
+		public override Vector2? HoldoutOffset() {
+			return new Vector2(2f, -2f);
+		}
+
+		//TODO: Move this to a more specifically named example. Say, a paint gun?
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+			// Every projectile shot from this gun has a 1/3 chance of being an ExampleInstancedProjectile
+			type = ModContent.ProjectileType<TornadoShotProjectile>();
+		}
+
+		/*
+		* Feel free to uncomment any of the examples below to see what they do
+		*/
+
+		// What if I wanted it to work like Uzi, replacing regular bullets with High Velocity Bullets?
+		// Uzi/Molten Fury style: Replace normal Bullets with High Velocity
+		/*public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+			if (type == ProjectileID.Bullet) { // or ProjectileID.WoodenArrowFriendly
+				type = ProjectileID.BulletHighVelocity; // or ProjectileID.FireArrow;
+			}
+		}*/
+
+		// What if I wanted multiple projectiles in a even spread? (Vampire Knives)
+		// Even Arc style: Multiple Projectile, Even Spread
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			float numberProjectiles = 5; // 3, 4, or 5 shots
+			float rotation = MathHelper.ToRadians(30);
+
+			position += Vector2.Normalize(velocity) * 45f;
+
+			for (int i = 0; i < numberProjectiles; i++) {
+				Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
+				Projectile.NewProjectile(source, position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+			}
+
+			return false; // return false to stop vanilla from calling Projectile.NewProjectile.
+		}
+
+		// How can I make the shots appear out of the muzzle exactly?
+		// Also, when I do this, how do I prevent shooting through tiles?
+		/*public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+			Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0)) {
+				position += muzzleOffset;
+			}
+		}*/
+
+		// How can I get a "Clockwork Assault Rifle" effect?
+		// 3 round burst, only consume 1 ammo for burst. Delay between bursts, use reuseDelay
+		// Make the following changes to SetDefaults():
+		/*
+			item.useAnimation = 12;
+			item.useTime = 4; // one third of useAnimation
+			item.reuseDelay = 14;
+			item.consumeAmmoOnLastShotOnly = true;
+		*/
+
+		// How can I shoot 2 different projectiles at the same time?
+		/*public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			// Here we manually spawn the 2nd projectile, manually specifying the projectile type that we wish to shoot.
+			Projectile.NewProjectile(source, position, velocity, ProjectileID.GrenadeI, damage, knockback, player.whoAmI);
+
+			// By returning true, the vanilla behavior will take place, which will shoot the 1st projectile, the one determined by the ammo.
+			return true;
+		}*/
+
+		// How can I choose between several projectiles randomly?
+		/*public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+			// Here we randomly set type to either the original (as defined by the ammo), a vanilla projectile, or a mod projectile.
+			type = Main.rand.Next(new int[] { type, ProjectileID.GoldenBullet, ModContent.ProjectileType<Projectiles.ExampleBullet>() });
+		}*/
+	}
+}
---
+++ Content/Projectiles/TornadoShotProjectile.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Build.Evaluation;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Projectiles
+{
+	// This Example show how to implement simple homing projectile
+	// Can be tested with ExampleCustomAmmoGun
+	public class TornadoShotProjectile : ModProjectile
+	{
+		// Setting the default parameters of the projectile
+		// You can check most of Fields and Properties here https://github.com/tModLoader/tModLoader/wiki/Projectile-Class-Documentation
+		public override void SetDefaults() {
+			Projectile.width = 8; // The width of projectile hitbox
+			Projectile.height = 8; // The height of projectile hitbox
+
+			Projectile.aiStyle = 0; // The ai style of the projectile (0 means custom AI). For more please reference the source code of Terraria
+			Projectile.DamageType = DamageClass.Ranged; // What type of damage does this projectile affect?
+			Projectile.friendly = true; // Can the projectile deal damage to enemies?
+			Projectile.hostile = false; // Can the projectile deal damage to the player?
+			Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
+			Projectile.light = 1f; // How much light emit around the projectile
+			Projectile.tileCollide = false; // Can the projectile collide with tiles?
+			Projectile.timeLeft = 600; // The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+		}
+
+		// Custom AI
+		public override void AI() {
+			Projectile.ai[0] += 1;
+			if (Projectile.ai[0] == 30)
+			{
+				float numberProjectiles = 5; 
+				float rotation = MathHelper.ToRadians(72); // 360/5
+				for (int i = 0; i < numberProjectiles; i++)
+				{
+					Vector2 perturbedSpeed = Projectile.velocity.RotatedBy(rotation * i); // Watch out for dividing by 0 if there is only 1 projectile.
+					Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.position, perturbedSpeed, Projectile.type, Projectile.damage, Projectile.knockBack, Projectile.owner, Projectile.ai[0]);
+					Dust.NewDustDirect(Projectile.position + new Vector2(8, 8), 102, 32, DustID.Cloud, 0.0f, 0.0f, 100, Color.WhiteSmoke, 10.0f);
+				}
+			}
+		}
+	}
+}
